6d1d764676c0162d360701413294e6ac0723b9de
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-20 06:12:38 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,5 @@
+let counterValue = 0;
+const buttonDecr = document.querySelector('button[data-action = decrement]');
+const buttonIncr = document.querySelector('button[data-action = increment]');
+const currentValue = document.querySelector('#value');
+
