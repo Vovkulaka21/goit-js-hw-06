f84e41ea227af331076eb170fbd9b7d31ba4b833
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-20 06:29:20 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,14 @@ const buttonDecr = document.querySelector('button[data-action = decrement]');
 const buttonIncr = document.querySelector('button[data-action = increment]');
 const currentValue = document.querySelector('#value');
 
+const clickDecrement = () => {
+    counterValue -= 1;
+    currentValue.textContent = counterValue
+};
+const clickIncrement = () => {
+    counterValue += 1;
+    currentValue.textContent = counterValue
+};
+
+buttonDecr.addEventListener('click', clickDecrement);
+buttonIncr.addEventListener('click', clickIncrement);
