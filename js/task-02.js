8ca914ccfdd0ca50c09daae62a7ce5e4dd790ed7
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-20 04:20:45 +0200

    task 2 add

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,17 @@ const ingredients = [
   'Condiments',
 ];
 
+const ingredientsEl = document.querySelector('#ingredients')
+
+
+
+const ingperEl = ingredients.map(el => {
+  
+  const navItemEl = document.createElement('li');
+  navItemEl.textContent = el;
+  navItemEl.classList.add('item');
+  return navItemEl
+
+})
+
+ingredientsEl.append(...ingperEl);
