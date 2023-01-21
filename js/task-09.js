779fbeb4511103feaefaaf6300e06df5f40fba85
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-21 02:29:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorButton = document.querySelector('.change-color');
+const colorName =  document.querySelector('.color');
+const backgroundColor = document.querySelector('body');
+
+colorButton.addEventListener('click', changeBgColorRandom);
+
+function changeBgColorRandom () {
+
+  
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = `${getRandomHexColor()}`
+
+}
