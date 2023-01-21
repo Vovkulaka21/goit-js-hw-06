239bb0ecc9ee478ea1d600e719247b13e88b9836
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-21 02:18:34 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', loginFormSubmit);
+
+function loginFormSubmit (event) {
+
+    event.preventDefault();
+
+    const mail = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value
+
+    if (mail === '' || password === '') {
+
+      alert('Please fill empty field')
+
+    }
+
+    const result = { mail: mail, password: password };
+
+    console.log(result);
+    
+    event.currentTarget.reset();
+}
